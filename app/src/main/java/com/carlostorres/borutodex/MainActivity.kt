13b109270471f2073a120cff4d596b8d8c64a8e4
Carlos Torres <kisamehatake33@comunidad.unam.mx>
2024-09-08 21:36:35 -0600

    SetupNavigation

--- app/src/main/java/com/carlostorres/borutodex/MainActivity.kt
+++ app/src/main/java/com/carlostorres/borutodex/MainActivity.kt
@@ -3,40 +3,25 @@ package com.carlostorres.borutodex
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.carlostorres.borutodex.navigation.SetupNavGraph
 import com.carlostorres.borutodex.ui.theme.BorutoDexTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var navController : NavHostController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BorutoDexTheme {
-                Greeting(
-                    name = "Android"
-                )
+
+                navController = rememberNavController()
+
+                SetupNavGraph(navController = navController)
+
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BorutoDexTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/carlostorres/borutodex/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.carlostorres.borutodex.util
+
+object Constants {
+
+    const val DETAILS_ARGUMENT_KEY = "heroID"
+
+}
