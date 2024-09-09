3dd14556a5faed31b664d8ad8be68a2f82214945
Carlos Torres <kisamehatake33@comunidad.unam.mx>
2024-09-08 22:09:46 -0600

    Adding TypeConverters

--- app/src/main/java/com/carlostorres/borutodex/data/local/BorutoDatabase.kt
+++ app/src/main/java/com/carlostorres/borutodex/data/local/BorutoDatabase.kt
@@ -2,12 +2,14 @@ package com.carlostorres.borutodex.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.carlostorres.borutodex.data.local.dao.HeroDao
 import com.carlostorres.borutodex.data.local.dao.HeroRemoteKeyDao
 import com.carlostorres.borutodex.data.model.Hero
 import com.carlostorres.borutodex.data.model.HeroRemoteKey
 
 @Database(entities = [Hero::class, HeroRemoteKey::class], version = 1)
+@TypeConverters(DatabaseConverter::class)
 abstract class BorutoDatabase : RoomDatabase(){
 
     abstract fun heroDao() : HeroDao
