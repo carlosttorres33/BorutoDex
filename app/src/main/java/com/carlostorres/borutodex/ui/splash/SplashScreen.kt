c50c2a9aab5d84873248a88bc8bc965aa596c3fd
Carlos Torres <kisamehatake33@comunidad.unam.mx>
2024-09-09 13:46:30 -0600

    AnimatedSplashScreen

--- app/src/main/java/com/carlostorres/borutodex/ui/splash/SplashScreen.kt
+++ app/src/main/java/com/carlostorres/borutodex/ui/splash/SplashScreen.kt
@@ -1,14 +1,19 @@
 package com.carlostorres.borutodex.ui.splash
 
 import android.content.res.Configuration
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -21,11 +26,29 @@ import com.carlostorres.borutodex.ui.theme.Purple700
 
 @Composable
 fun SplashScreen(navController: NavHostController) {
-    Splash()
+
+    val rotate = remember {
+        Animatable(0f)
+    }
+
+    LaunchedEffect(key1 = true) {
+        rotate.animateTo(
+            targetValue = 360f,
+            animationSpec = tween(
+                durationMillis = 1000,
+                delayMillis = 200
+            )
+        )
+    }
+
+    Splash(rotate = rotate.value)
+
 }
 
 @Composable
-fun Splash() {
+fun Splash(
+    rotate : Float
+) {
 
     if (isSystemInDarkTheme()) {
 
@@ -37,6 +60,8 @@ fun Splash() {
         ) {
 
             Image(
+                modifier = Modifier
+                    .rotate(degrees = rotate),
                 painter = painterResource(id = R.drawable.ic_logo),
                 contentDescription = stringResource(R.string.app_logo)
             )
@@ -57,6 +82,8 @@ fun Splash() {
         ) {
 
             Image(
+                modifier = Modifier
+                    .rotate(degrees = rotate),
                 painter = painterResource(id = R.drawable.ic_logo),
                 contentDescription = stringResource(R.string.app_logo)
             )
@@ -68,5 +95,5 @@ fun Splash() {
 @Composable
 @Preview (uiMode = Configuration.UI_MODE_NIGHT_YES)
 private fun SplashScreenPreview() {
-    Splash()
+    Splash(rotate = 0f)
 }
