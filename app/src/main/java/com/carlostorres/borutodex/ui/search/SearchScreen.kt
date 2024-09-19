8ddf44c4368b5f9cd1d4d943d968e0c56baa0885
Carlos Torres <kisamehatake33@comunidad.unam.mx>
2024-09-19 12:46:40 -0600

    Search Heroes Source

--- app/src/main/java/com/carlostorres/borutodex/ui/search/SearchScreen.kt
+++ app/src/main/java/com/carlostorres/borutodex/ui/search/SearchScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
